00038b4cc9fe5302eb92a826bc99bdc6037eb05d
Kumar Kartikay <kumarkartikay005@gmail.com>
2024-09-13 14:42:07 +0530

    completed backend 1

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connecToMongoDB.js";
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(cookieParser()); // to parse the incoming cookies from the request
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
 
 
 // app.get("/",(req,res) => {
