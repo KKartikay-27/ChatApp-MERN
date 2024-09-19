c4a6dc1b71abbb9d287c20a6005c42722c0677e7
Kumar Kartikay <kumarkartikay005@gmail.com>
2024-09-20 01:07:47 +0530

    build

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
@@ -9,6 +10,8 @@ import { app,server } from "./socket/socket.js";
 
 const port = process.env.PORT || 5000;
 
+const __dirname = path.resolve(); 
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body)
@@ -18,12 +21,11 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
- 
-// app.get("/",(req,res) => {
-//     // root route https://localhost:5000/
-//     res.send("Hello World ")
-// });
+app.use(express.static(path.join(__dirname, "/client/dist"))); 
 
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname, "frontend","dist","index.html"));
+});
 
 server.listen(port, () => {
     connectToMongoDB();
